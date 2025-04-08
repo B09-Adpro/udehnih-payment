5159f7bc56174ba2af5e747e884a5900207d8c43
AthallahD <athallah2209@gmail.com>
2025-04-08 17:17:21 +0700

    init commit (current progress

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihpayment/UdehnihPaymentApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdehnihPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdehnihPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/udehnihpayment/UdehnihPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdehnihPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

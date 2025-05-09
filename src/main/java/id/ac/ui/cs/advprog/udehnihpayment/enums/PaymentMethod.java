18a978f3dd83b7d1e9a268f6b9f26ed45777031b
AthallahD <athallah2209@gmail.com>
2025-05-09 19:56:07 +0700

    [REFACTOR] fix error for toString method

--- src/main/java/id/ac/ui/cs/advprog/udehnihpayment/enums/PaymentMethod.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihpayment/enums/PaymentMethod.java
@@ -1,24 +1,27 @@
 package id.ac.ui.cs.advprog.udehnihpayment.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PaymentMethod {
     BANK_TRANSFER("BankTransfer"),
     CREDIT_CARD("CreditCard");
-    
+
     private final String value;
-    
+
     private PaymentMethod(String value) {
         this.value = value;
     }
-    
+
     public String getValue() {
         return value;
     }
-    
+
     @Override
+    @JsonValue
     public String toString() {
         return value;
     }
-    
+
     public static boolean contains(String text) {
         for (PaymentMethod paymentMethod : PaymentMethod.values()) {
             if (paymentMethod.value.equalsIgnoreCase(text)) {
@@ -27,7 +30,7 @@ public enum PaymentMethod {
         }
         return false;
     }
-    
+
     public static PaymentMethod fromString(String text) {
         for (PaymentMethod paymentMethod : PaymentMethod.values()) {
             if (paymentMethod.value.equalsIgnoreCase(text)) {

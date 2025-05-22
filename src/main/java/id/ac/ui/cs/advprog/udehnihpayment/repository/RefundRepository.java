1c721e555671c955cdead78079adcb0e19fb9c2e
AthallahD <athallah2209@gmail.com>
2025-05-22 11:29:41 +0700

    feat: add new method on refundRepository

--- src/main/java/id/ac/ui/cs/advprog/udehnihpayment/repository/RefundRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihpayment/repository/RefundRepository.java
@@ -4,10 +4,12 @@ import id.ac.ui.cs.advprog.udehnihpayment.model.Refund;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RefundRepository extends JpaRepository<Refund, UUID> {
 
-    Refund findByTransactionId(UUID transactionId);
+    Refund findByPayment_TransactionId(UUID transactionId);
+    List<Refund> findAll();
 }

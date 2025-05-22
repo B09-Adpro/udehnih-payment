f578692e20b2c1f8a3b83aef2a5856dbbf18c435
AthallahD <athallah2209@gmail.com>
2025-05-22 11:28:33 +0700

    feat: delete unused method and add new method

--- src/main/java/id/ac/ui/cs/advprog/udehnihpayment/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihpayment/repository/PaymentRepository.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     List<Payment> findAllByUserId(Long userId);
-    List<Payment> findAllByPaymentStatus(String paymentStatus);
-    List<Payment> findAllByPaymentMethod(String paymentMethod);
+    List<Payment> findAll();
     Payment findByTransactionId(UUID transactionId);
 }

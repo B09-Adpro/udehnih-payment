acebabc62cd98a3de96b93f72d2cccd1869cd5a0
AthallahD <athallah2209@gmail.com>
2025-05-23 23:03:05 +0700

    feat: add exception handler

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihpayment/exception/UnauthorizedAccessException.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.udehnihpayment.exception;
+
+public class UnauthorizedAccessException extends RuntimeException {
+  public UnauthorizedAccessException(String message) {
+    super(message);
+  }
+}

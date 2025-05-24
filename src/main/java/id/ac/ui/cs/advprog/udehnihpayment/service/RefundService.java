e1633b1b959ec069f65cf35e95beff63063d20f5
AthallahD <athallah2209@gmail.com>
2025-05-25 02:02:42 +0700

    feat: new method for refund service

--- src/main/java/id/ac/ui/cs/advprog/udehnihpayment/service/RefundService.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihpayment/service/RefundService.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.udehnihpayment.service;
 
+import id.ac.ui.cs.advprog.udehnihpayment.enums.RefundStatus;
 import id.ac.ui.cs.advprog.udehnihpayment.model.Refund;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.List;
 public interface RefundService {
     Refund requestRefund(Long transactionId, String reason, String details);
     List<Refund> getAllRefunds();
+    Refund updateRefundStatus(Long refundId, RefundStatus status, String approvedBy);
+    Refund findById(Long refundId);
 }
